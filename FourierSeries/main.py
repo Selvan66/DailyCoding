b9d00452f564858230848c745f206c1b92e9d509
Rafał Szyiński <72799471+Selvan66@users.noreply.github.com>
2022-01-22 01:33:37 +0100

    Drawing with fourier series

--- FourierSeries/main.py
+++ FourierSeries/main.py
@@ -1,60 +1,78 @@
 import pygame as pg
 import math, sys
-from random import randint
-
 from dft import dft
 
+def readPointsFromFile(file):
+    p = []
+    with open(file) as f:
+        for line in f.readlines():
+            p.append(eval(line))
+    return p
+
+############### SETUP #####################
 pg.init()
 screen = pg.display.set_mode((800, 800))
 pg.display.set_caption('Fourier Series')
 
 clock = pg.time.Clock()
-
-num = 200
-points = []
-sygnal = [100 * math.cos(2 * math.pi * i * 1/num) for i in range(num)]
-fourier = dft(sygnal)
 time = 0
 
-while True:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            pg.quit()
-            sys.exit()
+num = 200
+path = []
 
-    screen.fill((0, 0, 0))
-            
-    circle_pos = (screen.get_width() / 4, screen.get_height() / 2)
-    x = 0
-    y = 0
-    
-    for i in range(num):
-        freq = fourier[i][2]
-        radius = fourier[i][3]
-        phase = fourier[i][4]
-        x = radius * math.cos(freq * time + phase + math.pi/2) + circle_pos[0]
-        y = radius * math.sin(freq * time + phase + math.pi/2) + circle_pos[1]
-        
-        
-        pg.draw.circle(screen, 'White', circle_pos, radius, 2)
-        pg.draw.line(screen, 'White', circle_pos, (x, y))
-        circle_pos = (x, y)
-        pg.draw.circle(screen , 'White', (x, y), 3)
-        
-        
-    points = [y] + points
-    if (len(points) > 300):
-        points.pop()
-        
-    for i in range(len(points) - 1):
-        pg.draw.aaline(screen, 'White', (i + 500, points[i]), (i + 501, points[i + 1]))
-    pg.draw.line(screen, 'White', (x, y), (500, points[0]), 2)
-    pg.display.update()
-    
-    dt = 2 * math.pi / num
-    time += dt
-    
-    if time > 2 * math.pi:
-        time = 0
-    
-    clock.tick(60)
+sygnal = [100 * math.sin(2 * math.pi * i * 3 * 1/num) for i in range(num)]
+fourierX = dft(sygnal)
+sygnal = [50 * math.cos(2 * math.pi * i * 5 * 1/num) for i in range(num)]
+fourierY = dft(sygnal)
+
+
+############################################
+
+def drawFourier(x, y, rotation, fourier):
+	num = len(fourier)
+	for i in range(num):
+		prev = (x, y)
+		freq = fourier[i][2]
+		radius = fourier[i][3]
+		phase = fourier[i][4]
+		x += radius * math.cos(freq * time + phase + rotation)
+		y += radius * math.sin(freq * time + phase + rotation)
+		
+		pg.draw.circle(screen, 'White', prev, radius, 2)
+		pg.draw.line(screen, 'White', prev, (x, y))
+		
+	return (x, y)
+	
+	
+	
+while True:
+	for event in pg.event.get():
+		if event.type == pg.QUIT:
+			pg.quit()
+			sys.exit()
+
+	screen.fill((0, 0, 0))
+			
+	vx = drawFourier(screen.get_width() / 2 + 100, 100, 0, fourierX) 
+	vy = drawFourier(100, screen.get_height() / 2 + 100, math.pi/2, fourierY) 
+	v = (vx[0], vy[1])
+	path.insert(0, v)
+	
+	if len(path) != 1:
+		pg.draw.lines(screen, 'White', False, path)
+	
+	pg.draw.line(screen, 'White', vx, v)
+	pg.draw.line(screen, 'White', vy, v)
+ 
+	if len(path) > 300:
+		path.pop()
+ 
+	dt = 2 * math.pi / num
+	time += dt
+	
+	if time > 2 * math.pi:
+		time = 0
+		path = []
+	
+	pg.display.update()
+	clock.tick(33)
