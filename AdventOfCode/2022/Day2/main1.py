54a5b556b8a00f7f51d07e4e21d688e8e74a6aa6
Rafał Szyiński <72799471+Selvan66@users.noreply.github.com>
2022-12-06 11:16:31 +0100

    Day2

---
+++ AdventOfCode/2022/Day2/main1.py
@@ -0,0 +1,24 @@
+
+def main():
+    point = 0
+    with open("data.txt") as file:
+        for line in file.readlines():
+            line = line.rstrip()
+            fight = line.split(" ")
+            oponent = fight[0]
+            you = fight[1]
+            match (oponent, you):
+                case ("A", "X"): point += 3 + 1
+                case ("A", "Y"): point += 6 + 2
+                case ("A", "Z"): point += 0 + 3
+                case ("B", "X"): point += 0 + 1
+                case ("B", "Y"): point += 3 + 2
+                case ("B", "Z"): point += 6 + 3
+                case ("C", "X"): point += 6 + 1
+                case ("C", "Y"): point += 0 + 2
+                case ("C", "Z"): point += 3 + 3
+    print(point)
+
+
+if __name__ == "__main__":
+    main()
