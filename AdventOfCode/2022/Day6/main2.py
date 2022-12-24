38fbc6a760fe2e2d626efd641cd44151114f4c37
Rafał Szyiński <72799471+Selvan66@users.noreply.github.com>
2022-12-24 08:54:39 +0100

    Day6/2

---
+++ AdventOfCode/2022/Day6/main2.py
@@ -0,0 +1,22 @@
+def main():
+    with open("data.txt") as file:
+        for line in file.readlines():
+            queue = []
+            result = None
+            for i, letter in enumerate(line):
+                if i < 14:
+                    queue.append(letter)
+                    continue
+
+                count = set(queue)
+                if len(count) > 13:
+                    result = i
+                    break
+
+                queue.append(letter)
+                queue.pop(0)
+            print(count, result)
+
+
+if __name__ == "__main__":
+    main()
