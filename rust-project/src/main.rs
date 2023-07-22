509bd3b9ebefbe314ca0e0b55f86b5012ad2188d
Rafał Szyiński <rafikop.pl@gmail.com>
2023-07-22 18:17:15 +0200

    part 2

--- rust-project/src/main.rs
+++ rust-project/src/main.rs
@@ -1,36 +1,30 @@
+mod theme;
+
+use dotenv::dotenv;
 use std::error::Error;
 
-use colour::{dark_green, yellow};
-use serde::Deserialize;
-
-#[derive(Deserialize, Debug)]
-struct Articles {
-    articles: Vec<Article>,
-}
-
-#[derive(Deserialize, Debug)]
-struct Article {
-    title: String,
-    url: String,
-}
-
-fn get_articles(url: &str) -> Result<Articles, Box<dyn Error>> {
-    let response = ureq::get(url).call()?.into_string()?;
-    let articles: Articles = serde_json::from_str(&response)?;
-    Ok(articles)
-}
+use newsapi::{get_articles, Articles};
 
 fn render_articles(articles: &Articles) {
-    for a in articles.articles.iter() {
-        dark_green!("> {}\n", a.title);
-        yellow!("> {}\n\n", a.url);
+    let theme = theme::default();
+    theme.print_text("# Top headlines\n\n");
+
+    for i in &articles.articles {
+        theme.print_text(&format!("`{}`", i.title));
+        theme.print_text(&format!("> *{}*", i.url));
+        theme.print_text("---");
     }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let url =
-        "https://newsapi.org/v2/top-headlines?country=pl&apiKey=c5381eeca8094120b0518f1872e4f7f0";
-    let articles = get_articles(url)?;
+    dotenv();
+
+    let api_key = std::env::var("API_KEY")?;
+
+    let url = "https://newsapi.org/v2/top-headlines?country=pl&apiKey=";
+    let url = format!("{}{}", url, api_key);
+
+    let articles = get_articles(&url)?;
 
     render_articles(&articles);
 
